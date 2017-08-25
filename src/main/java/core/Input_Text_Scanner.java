ea4d4ea50e87b75edbca26a93cd98152da882250
Wis13 <vkqa@outlook.com>
2017-08-25 06:28:05 -0700

    All work

---
+++ src/main/java/core/Input_Text_Scanner.java
@@ -0,0 +1,16 @@
+package core;
+import java.io.*;
+import java.util.Scanner;
+public class Input_Text_Scanner {
+	public static void main(String[] args) throws FileNotFoundException {
+		// String file = args[0];
+		File file = new File("input.txt");
+		Scanner s = new Scanner(file);
+		while (s.hasNext()) {
+			String first_name = s.next();
+			String last_name = s.next();
+			System.out.println("Full name is: " + first_name + " " + last_name);}
+		s.close();
+	}
+}
+
